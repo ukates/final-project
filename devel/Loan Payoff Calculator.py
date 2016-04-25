93a978672a1c3fd325d3570aa7bcb273f7c8c9bd
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-04-25 19:40:27 -0400

    Final Project 1st run

---
+++ devel/Loan Payoff Calculator.py
@@ -0,0 +1,67 @@
+import time, datetime
+print('WELCOME TO YOUR PERSONAL AUTO LOAN CALCULATOR!')
+print()
+while True:
+    try:
+        print('How much will you be borrowing? #NOTE# No need for commas! ')
+        p = float(input())
+    except ValueError:
+        print('Invalid input, please do not use commas!\n')
+    else:
+        break
+while True:
+    try:
+        print('What will be your interest rate? #NOTE# No % symbol needed!' )
+        rate = float(input())/100
+        r = (rate/int(12))
+    except ValueError:
+        print('Invalid input, please do not use % symbol!\n')
+    else:
+        break
+while True:
+    try:
+        print('In how many months would you like to pay this off? Integers only please!')
+        l = int(input())
+    except ValueError:
+        print('Invalid input, integers only please!\n')
+    else:
+        break
+while True:
+    try:
+        date_entry = input('Lastly, what will be the date of your first payment?\nPlease use the format YYYY-MM-DD:\n') 
+        year, month, day = map(int, date_entry.split('-'))
+        paymentDate= datetime.date(year,month,day)
+        some_day = datetime.timedelta(l*365/12)
+        payOffDate = paymentDate + some_day
+    except ValueError:
+        print('Invalid format, please use YYYY-MM-DD with dashes please!\n')
+    else:
+        break
+
+
+def payOff(p,r,l):
+    a = p*(r*(1+r)**l)
+    b = (((1+r)**l)-1)
+    c = (a/b)
+
+    totalPayoff = (c*l)
+    interestPaid = totalPayoff - p
+    print()
+    print('Calculating...')
+    print()
+    time.sleep(3)
+    print('In order to pay off your loan in ' + str(l) + ' months, you would need to pay $' + str(round(c,2)) + ' per month.')
+    print()
+    time.sleep(2)
+    print()
+    print('Once the loan is complete, you will have paid $' + str(round(interestPaid,2)) + ' in interest!')
+    print()
+    time.sleep(2)
+    print()
+    print('Based on the date of your first payment, you will complete the terms of your loan on ' + str(payOffDate) + ' having paid a total of $ ' + str(round(totalPayoff,2))) 
+
+    
+payOff(p,r,l)
+
+
+    
--- devel/README.md
+++ devel/README.md
@@ -1,3 +1,9 @@
 # Instructions for Running my Project
 
-Replace this text with the instructions your classmates will need to run your project. If your code needs any libraries using `pip` or any other tool, those instructions should be here as well. A complete stranger should be able to follow these instructions, so don't be afraid to include videos, screenshots, diagrams, text, whatever helps!
+Hello class! 
+
+My program is faily simple.  As it stands there is no need to download any libraries.  All  you will need to do is run the program and follow the prompts.  I have left instructions within the code that tells you the format that I am looking for when I ask for the users input.  I have also included ValueError exceptions, so if you are the type of person who has a hard time following instructions, the program shouldnt crash until you give it what it wants!. 
+
+The program will ask you for the amount of loan you have taken out, interest rate, and the number of months it will take you to payoff your loan.  After the program takes down the financial info, it will ask you for the date that you will make your first payment.  
+
+Once you have put in your date, the program will calculate the monthly payment required to payoff your loan in the designated months. It will then show you how much interest you will pay, and the total amount paid principle + interest. My program will even tell you the date you are exptected to pay off the loan, as long as you dont miss any payments of course! 
